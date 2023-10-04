4323cf42fd6ee6303a5019e70bf254d8f5e424b8
nekobit <>
2023-10-04 06:01:40 +0000

    use TK, seaerch from titledb.json file
    FossilOrigin-Name: fd644e9ff7a89b81c47455e1c640897fdf086d38c5b7894288e5422b1766b305

--- wiiu-toolbox.pl
+++ wiiu-toolbox.pl
@@ -1,63 +1,144 @@
 #!/usr/bin/env perl
 use warnings;
-no warnings 'experimental::smartmatch';
+no warnings ('experimental::smartmatch');
 use feature 'switch';
 use Tk;
+#use Tk::PNG;
+use Tk::FileSelect;
+use JSON;
 my $config_filename = "config.txt";
-my $win = MainWindow->new(-title=>"Wii U Toolbox");
-$win->configure(-background => '#505050');
-my $bg = '#505050';
-my $fg = '#f0f0f0';
+my $win = MainWindow->new(-title=>"Wii U Toolbox", -background=>"#505050");
+$win->geometry( "600x400" );
+$win->optionAdd("*borderWidth", 4);
+$win->optionAdd("*background", "#303030");
+$win->optionAdd("*foreground", "#dadada");
+$win->optionAdd("*borderColor", "#303030");
+$win->optionAdd("*relief", "flat");
+$win->optionAdd("*padY", "0");
 
-# my $CFG = (-background => $bg, -foreground => $fg);
+my $results_box;
+my $titledb = [];
 
 sub menu_edit_config
 {
 	`gedit $config_filename || kwrite $config_filename || xterm -e "nano $config_filename || vim $config_filename || vi $config_filename"`;
 }
 
-my $left = $win->Frame->pack(-side=>'left', -fill=>'both');
-my $list = $left->Listbox->pack(-fill=>'both', -expand=>1);
-my @list = ("Wii VC Inject", "Wii U WUP Downloader");
-$list->insert('end', @list);
-$left->Button(-text=>"Exit", -command => sub{exit})->pack(-side=>'left');
-$left->Button(
-	-text=>"Edit config",
-	-command => sub { menu_edit_config }
-)->pack(-side=>'right');
+# ... heh
+my $mainframe = $win->Frame(-background => "#303030");
+my @tabs = (
+	$mainframe->Frame,
+	$mainframe->Frame,
+	$mainframe->Frame,
+);
 
-my $right = $win->Frame->pack(-side=>'right', -fill=>'both', -expand=>1);
-$right->Button(-text=>"Open")->pack(-side=>'left');
-$right->Button(-text=>"Exit")->pack(-side=>'left');
-
-
-sub menu_wiiu_vc_inject
-{
-	
+sub show_tab {
+	foreach my $tab (@tabs)
+	{
+		$tab->packForget;
+	}
+	$tabs[shift]->pack(-fill=>'both', -expand=>1);
 }
 
-sub root_menu
+sub init_headers
 {
-	$sel = $_d->menu( title => 'Select an option',
-		list => [ 'Wii VC Inject', 'Inject a Retail Wii VC game',
-		          'Wii U WUP Downloading', 'Inject a Wii VC game',
-		          'Edit Config', 'Edit the config',
-		          'Exit', 'Exactly what it says on the box' ]);
-	given ($sel)
+	my $top_frame = $win->Frame(-background=>"#505050")->pack;
+	#$top_frame->grid();
+	my @header_btns = (
+		$top_frame->Button(
+			-text => "Injector",
+			-compound => 'top',
+			#-image => $win->Photo(-file=>'data/wii.png'),
+			-command => sub { show_tab 0; }
+		)->pack,
+		$top_frame->Button(
+			-text => "Downloader",
+			-compound => 'top',
+			#-image => $win->Photo(-file=>'data/wii.png'),
+			-command => sub { show_tab 1; }
+		)->pack,
+		$top_frame->Button(
+			-text => "Fetcher",
+			-command => sub { show_tab 2; }
+		)->pack,
+		$top_frame->Button(
+			-text => "Edit config",
+			-command => sub { menu_edit_config }
+		)->pack
+	);
+	
+	for (my $i = 0; $i < scalar @header_btns; ++$i)
 	{
-		when ('Wii U VC Inject') {
-			menu_wiiu_vc_inject;
-		}
-		when ('Edit Config') {
-			menu_edit_config;
-		}
-		when ('Exit') { exit }
-		when (0) { exit }
-		default {}
+		$header_btns[$i]->grid(-row => 0, -column => $i);
 	}
 }
+init_headers;
 
+sub parse_titlekeys_db
+{
+	my ($filename, $input) = @_;
+	my $cb = sub { $input->configure(-textvariable=>shift); };
+	open my $fh, '<', $filename or $cb->("Can't open file");
+	$titledb = decode_json(do { local $/; <$fh> });
+}
 
-# while (1) { root_menu }
+sub search_db
+{
+	my $input = shift;
+	my $input_val = $input->get;
 
+	$results_box->delete(0, 25565);
+	my @sorted = ();
+	foreach $data (@{$titledb})
+	{
+		push(@sorted, $data->{name});
+	}
+	my @matches = grep { /$input_val/i } @sorted;
+	$results_box->insert('end', @matches);
+}
+
+sub init_tab_injector
+{
+	my $tab = $tabs[shift];
+	my $search = $tab->Frame->pack;
+	my $search_input = $search->Entry->pack;
+	my $search_btn = $search->Button(-text=>"Search", -command=>sub{ search_db $search_input })->pack;
+	
+	$search_input->grid(-row => 0, -column => 0);
+	$search_btn->grid(-row=> 0, -column => 1);
+	
+	
+	$results_box = $tab->Scrolled('Listbox', -scrollbars=>'oe')->pack(-fill=>'both', -expand=>1);
+}
+
+sub init_tab_download
+{
+	my $tab = $tabs[shift];
+	$tab->Button(-text=>"lol")->pack;
+}
+
+sub init_tab_fetcher
+{
+	my $tab = $tabs[shift];
+	my $input = $tab->Entry()->pack;
+	my $selector = $tab->Button(
+		-text=>"Select...",
+		-command => sub {
+			$FSref = $tab->FileSelect(-directory => "./");
+			$file = $FSref->Show;
+			$input->configure(-textvariable=>"$file") if $file;
+			parse_titlekeys_db($file, $input) if $file;
+		}
+	)->pack;
+	
+	$input->grid(-row => 0, -column => 0);
+	$selector->grid(-row => 0, -column => 1);
+}
+
+init_tab_injector(0);
+init_tab_download(1);
+init_tab_fetcher(2);
+
+# Start!
+$mainframe->pack(-fill=>'both', -expand=>1);
 MainLoop
